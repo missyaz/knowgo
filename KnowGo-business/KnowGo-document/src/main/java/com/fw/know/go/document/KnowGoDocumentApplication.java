37390586f0444c9511b01fb512c892b1dcf6a03d
Leo <545280332@qq.com>
2025-10-31 17:52:55 +0800

    feat: 仿照项目结构

---
+++ KnowGo-business/KnowGo-document/src/main/java/com/fw/know/go/document/KnowGoDocumentApplication.java
@@ -0,0 +1,18 @@
+package com.fw.know.go.document;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Classname KnowGoDocumentApplication
+ * @Description TODO
+ * @Date 29/10/2025 上午11:23
+ * @Author  Leo
+ */
+@SpringBootApplication(scanBasePackages = {"com.fw.know.go.document"})
+public class KnowGoDocumentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowGoDocumentApplication.class, args);
+    }
+}
---
+++ KnowGo-business/KnowGo-document/src/main/java/com/fw/know/go/document/controller/DocumentController.java
@@ -0,0 +1,38 @@
+package com.fw.know.go.document.controller;
+
+import com.fw.know.go.file.FileService;
+import com.fw.know.go.web.vo.Result;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.InputStream;
+
+/**
+ * @Classname DocumentController
+ * @Description 文档控制器
+ * @Date 29/10/2025 下午1:16
+ * @Author Leo
+ */
+@Slf4j
+@RestController
+@RequestMapping("/document")
+@RequiredArgsConstructor
+public class DocumentController {
+
+    private final FileService fileService;
+
+    @PostMapping("/parseDocument")
+    public Result<Void> parseDocument(@RequestParam("file")MultipartFile file) {
+        try {
+            String text = fileService.extractText(file.getInputStream());
+            return Result.success();
+        } catch (Exception e) {
+            return Result.error();
+        }
+    }
+}
