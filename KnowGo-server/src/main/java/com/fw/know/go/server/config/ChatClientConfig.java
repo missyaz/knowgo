278774856fe1f6895a7335b637984309e558b3cb
Leo <545280332@qq.com>
2025-11-05 22:53:59 +0800

    feat: 模仿项目结构，实现解析文档接口

--- KnowGo-server/src/main/java/com/fw/know/go/server/config/ChatClientConfig.java
+++ KnowGo-server/src/main/java/com/fw/know/go/server/config/ChatClientConfig.java
@@ -1,9 +1,20 @@
 package com.fw.know.go.server.config;
 
 import com.alibaba.cloud.ai.dashscope.api.DashScopeApi;
+import com.alibaba.cloud.ai.dashscope.embedding.DashScopeEmbeddingModel;
+import com.alibaba.cloud.ai.dashscope.embedding.DashScopeEmbeddingOptions;
+import io.micrometer.observation.ObservationRegistry;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chroma.vectorstore.ChromaApi;
+import org.springframework.ai.chroma.vectorstore.ChromaVectorStore;
+import org.springframework.ai.embedding.BatchingStrategy;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.chroma.autoconfigure.ChromaVectorStoreProperties;
+import org.springframework.ai.vectorstore.observation.VectorStoreObservationConvention;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,13 +27,32 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ChatClientConfig {
 
+    @Value("${spring.ai.dashscope.api-key}")
+    private String apiKey;
+
     @Bean
     public ChatClient chatClient(ChatClient.Builder builder) {
         return builder.defaultAdvisors(new SimpleLoggerAdvisor()).build();
     }
 
     @Bean
-    public DashScopeApi dashScopeApi(@Value("${spring.ai.dashscope.api-key}") String apiKey) {
+    public DashScopeApi dashScopeApi() {
         return DashScopeApi.builder().apiKey(apiKey).build();
+    }
+
+    @Bean
+    public ChromaVectorStore vectorStore(EmbeddingModel embeddingModel, ChromaApi chromaApi,
+                                         ChromaVectorStoreProperties storeProperties, ObjectProvider<ObservationRegistry> observationRegistry,
+                                         ObjectProvider<VectorStoreObservationConvention> customObservationConvention,
+                                         BatchingStrategy chromaBatchingStrategy) {
+        return ChromaVectorStore.builder(chromaApi, embeddingModel)
+                .tenantName(storeProperties.getTenantName())
+                .databaseName(storeProperties.getDatabaseName())
+                .collectionName(storeProperties.getCollectionName())
+                .initializeSchema(storeProperties.isInitializeSchema())
+                .observationRegistry(observationRegistry.getIfUnique(() -> ObservationRegistry.NOOP))
+                .customObservationConvention(customObservationConvention.getIfAvailable(() -> null))
+                .batchingStrategy(chromaBatchingStrategy)
+                .build();
     }
 }
--- KnowGo-server/src/main/java/com/fw/know/go/server/controller/AiController.java
+++ KnowGo-server/src/main/java/com/fw/know/go/server/controller/AiController.java
@@ -1,15 +1,24 @@
 package com.fw.know.go.server.controller;
 
 import com.alibaba.cloud.ai.dashscope.api.DashScopeApi;
+import com.alibaba.cloud.ai.dashscope.embedding.DashScopeEmbeddingOptions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.embedding.Embedding;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.embedding.EmbeddingRequest;
+import org.springframework.ai.embedding.EmbeddingResponse;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @Classname AiController
@@ -29,9 +38,15 @@ public class AiController {
 
     private final ChatModel chatModel;
 
-    public AiController(ChatClient chatClient, ChatModel chatModel) {
+    private final EmbeddingModel embeddingModel;
+
+    private final VectorStore vectorStore;
+
+    public AiController(ChatClient chatClient, ChatModel chatModel, EmbeddingModel embeddingModel, VectorStore vectorStore) {
         this.chatClient = chatClient;
         this.chatModel = chatModel;
+        this.embeddingModel = embeddingModel;
+        this.vectorStore = vectorStore;
     }
 
     @GetMapping("/chat")
@@ -41,5 +56,16 @@ public class AiController {
                回答不能超过200字
                """;
         return chatModel.call(new Prompt(message)).getResult().getOutput().getText();
+    }
+
+    @GetMapping("/text2embedding")
+    public EmbeddingResponse chatWithVectorStore(@RequestParam(value = "message", defaultValue = "你是谁") String message) {
+//        EmbeddingResponse call = embeddingModel.call(new EmbeddingRequest(List.of(message), null));
+        EmbeddingResponse call = embeddingModel.call(new EmbeddingRequest(List.of(message),
+                DashScopeEmbeddingOptions.builder().withModel("text-embedding-v1").build()));
+
+        System.out.println(Arrays.toString(call.getResult().getOutput()));
+        return call;
+
     }
 }
