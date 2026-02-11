297ad24dccdc546bab04f0655e13a555149354d0
Leo <545280332@qq.com>
2026-02-11 17:59:22 +0800

    fix: 使用Dubbo进行服务注册

--- KnowGo-business/KnowGo-notice/src/main/java/com/fw/know/go/notice/KnowGoNoticeApplication.java
+++ KnowGo-business/KnowGo-notice/src/main/java/com/fw/know/go/notice/KnowGoNoticeApplication.java
@@ -1,5 +1,6 @@
 package com.fw.know.go.notice;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date 2025/11/3 20:44
  * @Description
  */
+@EnableDubbo
 @SpringBootApplication(scanBasePackages = {"com.fw.know.go.notice"})
 public class KnowGoNoticeApplication {
 
